cd9eec267528d33b0bc0d7fd84270261b7f31617
ROHITchin995 <rohitsure.9915@gmail.com>
2022-05-07 13:29:33 +0530

    first comit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { Suspense } from 'react';
+import Login from './pages/login';
+
+
+function App() {
+  return (
+    
+      <Router>
+        <Routes>
+          <Route path="/" element={
+            <Suspense fallback={<div className='loader'>Loading...</div>}>
+              <Login />
+            </Suspense>
+          } />
+        </Routes>
+      </Router>
+    
+  );
+}
+
+export default App;
