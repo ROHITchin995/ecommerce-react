869271651373879d554b81d5392f22ed4c8ac4af
ROHITchin995 <rohitsure.9915@gmail.com>
2022-05-09 21:29:39 +0530

    improved home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,37 +3,44 @@ import Navbar from "../components/Navbar";
 function Home(){
     return(
         <div>
-            <Navbar />
-            <div className="container py-5">
-                <div className="row">
-                    <div className="col">
-                        <h2 className="text-center">Welcome to Ecommerce</h2>
-                        <p>Choose a category to start shopping now</p>
+        <Navbar />
+        <div className="container py-5">
+            <div className="row">
+                <div className="col">
+                    <h2 className='text-center'>Welcome to ecommerce</h2>
+                    <p className='text-center'>Choose a category to start shopping now!</p>
 
-                        <div className="row m-5">
-                            <div className="col" />
-                                <div className="card py-5 rounded-3 bg-dark text-white">
-                                    <h5>Electronics</h5>
-                                </div>
-                                <div className="col" />
-                                <div className="card py-5 rounded-3 bg-dark text-white">
-                                    <h5>Fashion</h5>
-                                </div>
-                                <div className="col" />
-                                <div className="card py-5 rounded-3 bg-dark text-white">
-                                    <h5>Household</h5>
-                                </div>
-                                <div className="col" />
-                                <div className="card py-5 rounded-3 bg-dark text-white">
-                                    <h5>Appliance</h5>
-                                </div>
+                    <div className="row m-5">
+                        <div className="col ">
+                            <div className="card py-5 bg-dark text-white rounded-3">
+
+                                <h5 className='text-center'>Electronics</h5>
                             </div>
                         </div>
-                    </div>
-                
+                        <div className="col ">
+                            <div className="card py-5 bg-dark text-white rounded-3">
 
+                                <h5 className='text-center'>Fashion</h5>
+                            </div>
+                        </div>
+                        <div className="col ">
+                            <div className="card py-5 bg-dark text-white rounded-3">
+
+                                <h5 className='text-center'>Household</h5>
+                            </div>
+                        </div>
+                        <div className="col ">
+                            <div className="card py-5 bg-dark text-white rounded-3">
+
+                                <h5 className='text-center'>Appliances</h5>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
             </div>
         </div>
+    </div>
     )
 }
 
